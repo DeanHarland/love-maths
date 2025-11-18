479955b115549bd53beb491770cf874ff20aa80f
DeanHarland <mrdeanharland@gmail.com>
2025-11-18 15:40:34 +0000

    Set up structure of game functions.

---
+++ assets/js/script.js
@@ -0,0 +1,29 @@
+function runGame() {
+
+}
+
+function checkScore(){
+
+}
+
+function calculateCorrectAnswers(){
+
+}
+
+function incrementScore(){
+
+}
+
+function incrementWrongAnswer(){
+
+}
+
+function displayAdditionAnswer(){
+
+}
+function displaySubtractAnswer(){
+
+}
+function displayMultiplyAnswer(){
+
+}
